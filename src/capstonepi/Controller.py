65bf72e9a9690da5173c8b0980bad01b977582db
Amine Benaceur <abenaceur12@gmail.com>
2021-01-28 00:09:57 -0700

    added thermomemeter class

---
+++ src/capstonepi/Controller.py
@@ -0,0 +1,4 @@
+'''
+2020-01-28 AB: 
+    controller class to respond to keyboard, update temperature and launch PID thread.
+'''
--- src/capstonepi/Hardware_Thermometer.py
+++ src/capstonepi/Hardware_Thermometer.py
@@ -23,12 +23,72 @@ import time
 import board
 import busio
 import adafruit_mcp9600
+i2c = busio.I2C(board.SCL, board.SDA, frequency=10000)
+
+from threading import Thread, Lock
+
+
+class ProbeThermometer():
+
+    def __init__(self):
+        
+       self.i2c = busio.I2C(board.SCL, board.SDA, frequency = 100000)
+       self.mcp = adafruit_mcp9600.MCP9600(i2c)
+       
+    
+       self.probe_temp = 0
+       self.ambient_temp = 0
+       self.delta_Temp = 0
+       
+       self.mutex= Lock()
+
+    def set_temp(self, probe, ambient, delta):
+        self.mutex.acquire()
+
+        self.probe_temp = probe
+        self.ambient_temp = ambient
+        self.delta_temp = delta
+        
+        self.mutex.release()
+
+    def get_reading(self):
+        reading = {}
+        self.mutex.acquire()
+        try:
+            reading['probe'] = self.mcp.temperature
+            reading['ambient'] = self.mcp.ambient_temperature
+            reading['delta'] = self.mcp.delta_temperature
+        except:
+            print("Error in getting reading from probe")
+
+        self.mutex.release()
+        return reading
+
+
+
+        '''
+        self.probe_temp = self.mcp.temperature
+    
+        
+        '''
+
+
+probe = ProbeThermometer()
+while True:
+    read = probe.get_reading()
+    print("T= {} A= {} D={} ".format(read['probe'], read['ambient'], read['delta']))
+    time.sleep(1)
+
+
+
 # frequency must be set for the MCP9600 to function.
 # If you experience I/O errors, try changing the frequency.
+
+'''
 i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 mcp = adafruit_mcp9600.MCP9600(i2c)
 while True:
     print((mcp.ambient_temperature, mcp.temperature, mcp.delta_temperature))
     time.sleep(1)
 
-
+'''
