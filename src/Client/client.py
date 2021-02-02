f0f1eb1a2f99e18b267a7bdcd959b43e12573e5e
rnward0 <49202674+rnward0@users.noreply.github.com>
2021-02-02 14:00:45 -0700

    Client server ping

---
+++ src/Client/client.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+from __future__ import print_function
+
+from twisted.internet import task
+from twisted.internet.defer import Deferred
+from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineReceiver
+import time
+
+
+
+class Client(LineReceiver):
+    end = b'ending'
+
+    def connectionMade(self):
+        self.ping()
+        self.sendLine(b"Client connected")
+
+    def ping(self):
+        start = time.time()
+        self.transport.write(b'')
+        elapsed = time.time() - start
+        print('ping: ', elapsed)
+        
+    def lineReceived(self, line):
+        print("receive:", line)
+        if line == self.end:
+            self.transport.loseConnection()
+
+
+
+class EchoClientFactory(ClientFactory):
+    protocol = EchoClient
+
+    def __init__(self):
+        self.done = Deferred()
+
+
+    def clientConnectionFailed(self, connector, reason):
+        print('connection failed:', reason.getErrorMessage())
+        self.done.errback(reason)
+
+
+    def clientConnectionLost(self, connector, reason):
+        print('connection lost:', reason.getErrorMessage())
+        self.done.callback(None)
+
+
+
+def main(reactor):
+    factory = EchoClientFactory()
+    reactor.connectTCP('localhost', 8000, factory)
+    return factory.done
+
+
+
+if __name__ == '__main__':
+    task.react(main)
