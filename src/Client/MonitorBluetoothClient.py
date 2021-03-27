eac8561da5f56735cae3ad90a679af7a3d905aff
AmineBenaceur <abenaceur12@gmail.com>
2021-03-27 02:43:45 -0600

    experimenting...

--- src/Client/MonitorBluetoothClient.py
+++ src/Client/MonitorBluetoothClient.py
@@ -3,13 +3,13 @@ AB: simple Python script to send data to a server over bluetooth/
 '''
 import socket
 
-serverMACAdress = 'b8:27:eb:2a:e8:a0'
- port = 3
- s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
- s.connect((serverMACAddress,port))
- while 1:
-     text = input()
-         if text == "quit":
-                 break
-                     s.send(bytes(text, 'UTF-8'))
-                     s.close()
+serverMACAddress = 'b8:27:eb:2a:e8:a0'
+port = 3
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((serverMACAddress,port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+        s.send(bytes(text, 'UTF-8'))
+        s.close()
